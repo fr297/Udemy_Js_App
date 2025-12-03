fb98d5dd6f2fa6a7476dc6105ab9cda76c21bfd8
Alex Frolov <fr.280907@gmail.com>
2025-12-03 14:17:11 +0100

    initial commit

---
+++ test.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// const result = confirm("Are you here");
+// console.log(result);
+
+// const answer = prompt("you have 18?", "18");
+// console.log(typeof answer);
+
+// const answers = [];
+
+// answers[0] = prompt("What is your name?");
+// answers[1] = prompt("What is your surname?");
+// answers[2] = prompt("How old are you?");
+// document.write(answers);
+// console.log(typeof answers);
+
+let incr = 10,
+  decr = 10;
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+const isChecked = true,
+  isClose = false;
+
+console.log(isChecked || !isClose);
+
+console.log(2 + 2 * 2 !== 8);
+
+prompt("what is your name?", "");
