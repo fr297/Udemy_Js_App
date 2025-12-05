346af1dfd2451857df03fb79c57446a9ad4150ff
Alex Frolov <fr.280907@gmail.com>
2025-12-06 00:10:22 +0100

    fix conflict

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-<<<<<<< HEAD
 let numberOfFilms;
 
 function start() {
@@ -12,9 +11,6 @@ function start() {
 }
 
 start();
-=======
-const numberOfFilms = +prompt("How many films have you watched?", "");
->>>>>>> d482ed8ce99bb157e977aa1841eceee7941e0c2d
 
 const personalMovieDB = {
   count: numberOfFilms,
